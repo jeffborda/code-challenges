4b529f243ce33591b52853c1b78efff5a17f7412
Jeff Borda <jeffborda@outlook.com>
2018-09-10 20:49:57 -0700

    4 complete

--- forEach/challenges-01.test.js
+++ forEach/challenges-01.test.js
@@ -64,12 +64,12 @@ const addCurve = (arr) => {
 // ------------------------------------------------------------------------------------------------
 
 const greeting = (word) => {
-  // Solution code here...
-}
+  return word.toUpperCase();
+};
 
 const speaker = (message, callback) => {
-  // Solution code here...
-}
+  return callback(message);
+};
 
 // ------------------------------------------------------------------------------------------------
 // CHALLENGE 5
