c8d8722dd043a82a5b1ce3c0cae8de088f07821d
Jeff Borda <jeffborda@outlook.com>
2018-09-12 17:49:52 -0700

    4 finished

--- two-dimensional-arrays/challenges-03.test.js
+++ two-dimensional-arrays/challenges-03.test.js
@@ -94,8 +94,16 @@ const giveValentines = (list) => {
 const nestedArray = [ [ [1, 2, 3], [4, 5, 6] ], [ [7, 8, 9], [10, 11, 12] ], [ [13, 14, 15], [16, 17, 18] ] ];
 
 const findFourteen = (array) => {
-  // Solution code here...
-}
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array[i].length; j++) {
+      for(let k = 0; k < array[i][j].length; k++) {
+        if(array[i][j][k] === 14) {
+          return array[i][j][k];
+        }
+      }
+    }
+  }
+};
 
 // ------------------------------------------------------------------------------------------------
 // CHALLENGE 5
