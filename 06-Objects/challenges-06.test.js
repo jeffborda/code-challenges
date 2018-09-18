05936e854057806c94f06ea662bf6f0fe362dab1
Jeff Borda <jeffborda@outlook.com>
2018-09-17 20:37:40 -0700

    4 finished

--- 06-Objects/challenges-06.test.js
+++ 06-Objects/challenges-06.test.js
@@ -100,8 +100,16 @@ hasChildrenValues(characters, 'Eddard') will return false
 ------------------------------------------------------------------------------------------------*/
 
 const hasChildrenValues = (arr, character) => {
-  // Solution code here...
-}
+  arr.forEach(elem => {
+
+    if(elem.name === character) {
+      if(Object.values(elem)[2] === 'undefined'){
+        return false;
+      }
+    }
+  });
+  return true;
+};
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
@@ -185,10 +193,11 @@ describe('Testing challenge 3', () => {
   });
 });
 
-// describe('Testing challenge 4', () => {
-//   test('It should return true for characters that have children', () => {
-//     expect(hasChildrenValues(characters, 'Daenarys')).toBeTruthy();
-//   });
+describe('Testing challenge 4', () => {
+  test('It should return true for characters that have children', () => {
+    expect(hasChildrenValues(characters, 'Daenarys')).toBeTruthy();
+  });
+});
 
 //   test('It should return false to characters who do not have children', () => {
 //     expect(hasChildrenValues(characters, 'Sansa')).toBeFalsy();
